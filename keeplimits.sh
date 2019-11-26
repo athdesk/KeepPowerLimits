b06f55ea53f8a9bf7cad49b45300e787cedc3477
Mario Del Gaudio <47639916+athdesk@users.noreply.github.com>
2019-11-26 15:49:24 +0100

    adds reapply functionality

--- keeplimits.sh
+++ keeplimits.sh
@@ -8,16 +8,19 @@ ARGS_DOWN="powerlimit 15 28 44 0.002"
 timeout=30
 setval=0
 daemonval=0
+reapplyval=0
 
 usage() {
-	echo "Usage: keeplimits [-d] [-t <timeout>] [-s <up|down>] [-n] [-h]"
-	echo "	[-d]: explicitly runs the daemon"
+	echo "Usage: keeplimits [-d] [-t <timeout>] [-s <up|down>] [-r] [-h]"
+	echo "	[-d]: explicitly runs the daemon, ignores -r"
 	echo "	[-t]: sets the daemon timeout period in seconds (default = 30), and explicitely runs the daemon"
 	echo "	[-s]: sets and applies the specified setting"
+        echo "	[-r]: tries to reapply existing settings"
 	echo "	[-h]: shows this help"
 	echo ""
 	echo "If no parameters are passed, the program will act as if only -d was passed"
-        echo "This program works best if automatically run by root at startup"
+        echo "This program works best if automatically run by root at startup if limits are resetting randomly,"
+	echo "or if run automatically after sleep (using -r) if limits are resetting only after resuming"
 	exit 0
 }
 
@@ -53,11 +56,12 @@ setpl() {
 }
 
 
-while getopts ":hns:t:d" opt; do
+while getopts ":hrs:t:d" opt; do
 	case ${opt} in
 		t) timeout=${OPTARG}; daemonval=1;;
 		s) setval=${OPTARG};;
 		d) daemonval=1;;
+		r) reapplyval=1;;
 h | * | ?) usage;;
 	esac
 done
@@ -70,3 +74,4 @@ fi
 touch $TMPFILE_PATH
 if ! [ "$setval" = "0" ]; then setpl $setval; fi
 if [ $# -eq 0 ] || [ $daemonval -eq 1 ]; then daemon; fi
+if [ $reapplyval -eq 1 ]; then reapply; fi
